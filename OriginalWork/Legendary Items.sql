4a5bd7bae4eb62d7c3ee0f7c320bfe31f6f81bc5
ColeMcGregor <gc28mcgr@siena.edu>
2025-09-23 16:25:01 -0400

    Apply .gitignore (stop tracking venv, caches, etc.)

--- OriginalWork/Legendary Items.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE "LegendItem" (
-	"itemID"	INTEGER UNIQUE,
-	"name"	TEXT NOT NULL,
-	"price"	INTEGER,
-	"attune"	TEXT NOT NULL,
-	PRIMARY KEY("itemID" AUTOINCREMENT),
-	CHECK("attune" IN ('Y', 'N'))
-);
-
--- README.md
+++
@@ -1,2 +0,0 @@
-# Magic-Item-Database
-A database for storing all magic items for D&amp;D 5e for use in generating random loot or shop inventories. 
